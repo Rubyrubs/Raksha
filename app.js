c602e9785fd746b24c13773d4a867d50b442cf50
Rubyrubs <69633868+Rubyrubs@users.noreply.github.com>
2020-10-09 19:26:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+(function(){
+      let inputItem= document.querySelector('#input');
+       inputItem.onkeyup= function(e) {
+        if (e.keyCode===13){
+            if(inputItem.value){
+                addItem(inputItem);
+
+            }
+        }
+    };
+            
+    function addItem(Item){
+        let ul= document.querySelector('#list');
+        let li= document.createElement('li');
+        let textNode= document.createTextNode(inputItem.value);
+        li.appendChild(textNode);
+        ul.appendChild(li);
+        inputItem.value='';
+        li.onclick= function(e){
+            e.target.remove();
+        };
+    
+    }
+})();
